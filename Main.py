470882694c29e21048cceb1740772dd623de6b09
Arman Salimi <bobarmansalimi@gmail.com>
2022-05-12 19:09:54 +0430

    Chance is now calculated based on the secret word and you can now choose to play again or not

--- Main.py
+++ Main.py
@@ -1,26 +1,46 @@
 from os import system
+from keyboard import is_pressed
 
-theWord = input("The secret word: ")
-system("cls")
+while True:
+    system("cls")
+    theWord = input("The secret word: ")
+    system("cls")
 
-guessedLetters = []
-for chance in range(5):
-    #Gets what user has guessed
-    userGuess = input("Your guess: ").lower()
-    #defines letter index for putting the thing into a list
-    letterIndex = 0
-    #checks if any of the is in the word
-    for letter in theWord:
-        if letter.lower() == userGuess:
-            guessedLetters.insert(letterIndex, userGuess)
-        letterIndex += 1
-    print(guessedLetters)
-    #converts the guessed letters list to string
-    strGuessedLetters = ""
-    for letter in guessedLetters:
-        strGuessedLetters += str(letter)
-    #checks if you guessed the hole word
-    if strGuessedLetters == theWord.lower():
-        print("You won!")
-        exit()
-print("Game over!")
+    #Initializes the needed variables
+    wonOrNot = False
+    guessedLetters = []
+    #Game's loop
+    for chance in range(int(len(theWord)*1.5)):
+        #Gets what user has guessed
+        userGuess = input("Your guess: ").lower()
+        #Defines letter index for putting the thing into a list
+        letterIndex = 0
+        #Checks if any of the is in the word
+        for letter in theWord:
+            if letter.lower() == userGuess:
+                guessedLetters.insert(letterIndex, userGuess)
+            letterIndex += 1
+        print(guessedLetters)
+        #Converts the guessed letters list to string
+        strGuessedLetters = ""
+        for letter in guessedLetters:
+            strGuessedLetters += str(letter)
+        #Checks if you guessed the hole word
+        if strGuessedLetters == theWord.lower():
+            wonOrNot = True
+            #Tells the player status if the player lost
+            if wonOrNot:
+                print("You won!")  
+            break
+    #Tells the player status if the player won
+    if wonOrNot == False:
+        print("Game over!")
+
+    print("\nPlay again? press s\nToo tired? press e")
+    
+    #Checks the keys so it figures out if the player wants to play again
+    while True:
+        if is_pressed("s"):
+            break
+        if is_pressed("e"):
+            exit()
